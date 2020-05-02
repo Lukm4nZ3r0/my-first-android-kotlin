c183c1f60a6f8f47bebaab95ef48a96026ebf796
lukm4nz3r0 <laruku.asep@gmail.com>
2020-05-02 22:28:41 +0700

    add fab button

--- app/src/main/java/com/example/belajar/MainActivity.kt
+++ app/src/main/java/com/example/belajar/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.belajar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,7 +13,6 @@ class MainActivity : AppCompatActivity() {
 
     var currentPage: Int = 1
     var totalPage: Int = 0
-    var isLoading: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,25 +23,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        fetchJson(1)
+        fetchJson(1,this)
         prevNextButtonListener()
     }
 
     private fun prevNextButtonListener(){
         prev_button.setOnClickListener {
             if(currentPage > 1){
-                fetchJson(currentPage-1)
+                fetchJson(currentPage-1, this)
             }
         }
 
         next_button.setOnClickListener {
             if(currentPage < totalPage) {
-                fetchJson(currentPage+1)
+                fetchJson(currentPage+1, this)
             }
         }
     }
 
-    private fun fetchJson(page: Int){
+    private fun fetchJson(page: Int, mainActivity: MainActivity){
         val url = "https://reqres.in/api/users?page=${page}"
         val request = Request
             .Builder()
@@ -50,15 +50,15 @@ class MainActivity : AppCompatActivity() {
             .build()
         val client = OkHttpClient()
 
-        isLoading = true
-        loading_text.text = "Now Loading ..."
-
         var dummyDataObject: List<DataObject> = emptyList()
         var dummyAddObject = AdObject("","","")
         var dummyHomeModel = HomeModel(0,0,0,0,dummyDataObject,dummyAddObject)
 
         my_recycler_view.adapter = MainAdapter(dummyHomeModel, true)
 
+
+        mainActivity.next_button.isEnabled = false
+        mainActivity.prev_button.isEnabled = false
         client.newCall(request).enqueue(object: Callback {
             override fun onResponse(call: Call, response: Response) {
                 val body = response?.body?.string()
@@ -70,18 +70,28 @@ class MainActivity : AppCompatActivity() {
 
                 totalPage = homeModel.total_pages
                 currentPage = page
-                isLoading = false
-                loading_text.text = ""
 
                 runOnUiThread{
+                    mainActivity.next_button.isEnabled = true
+                    mainActivity.prev_button.isEnabled = true
                     my_recycler_view.adapter = MainAdapter(homeModel, false)
                     my_recycler_view.scheduleLayoutAnimation()
                 }
             }
             override fun onFailure(call: Call, e: IOException) {
                 println("GAGAL MENDAPATKAN REQUEST")
-                isLoading = false
-                loading_text.text = ""
+                val alertDialog: AlertDialog.Builder = AlertDialog.Builder(mainActivity)
+                runOnUiThread {
+                    alertDialog.setTitle("Try to connecting again!")
+                    alertDialog.setMessage("Please wait ..!")
+                    alertDialog.setNeutralButton("OK!"){ _, _ ->
+
+                    }
+                    alertDialog.show()
+
+//                    recursive
+                    fetchJson(page, mainActivity)
+                }
             }
         })
     }
